1c1f02cf89906fed3288dd91ec7d3e200009bb33
Shivanshu Karn <karnshivanshu2005@gmail.com>
2025-02-07 16:16:37 +0530

    removed callback url after login

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -16,16 +16,12 @@ export default function LoginPage() {
 
   const onSubmit = async (data) => {
     const result = await signIn('credentials', {
-      redirect: false,
-      callbackUrl: '/dashboard',
       ...data
     });
     
     if (result?.error) {
       toast.error(result.error);
-    } else {
-      router.replace(result?.url || '/dashboard');
-    }
+    } 
   };
 
   return (
